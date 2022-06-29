16e4089e2ec69fb8711881fcb4179a26fad6a16f
Golldan <cristiano.henriqueferreirabp@gmail.com>
2022-06-28 22:22:38 -0300

    feat: responsive navbar on scroll

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,17 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import PostTemplate from "../../components/home/posts/postTemplate";
 import MenuItems from "../../components/home/userMenu/menuItems";
 
 export default function Home() {
+
+    const [scroll_height, set_scroll_height] = useState(0);
+
+    const handle_nav_bar = () => {
+        const height = document.querySelector('#post-window');
+        if (height == undefined || height == null) return;
+        set_scroll_height(height.scrollTop);
+    }
+
     return (
         <Fragment>
             <main className="flex">
@@ -30,10 +39,12 @@ export default function Home() {
                             
                         </div>
                     </div>
-                    <div className="bg-zinc-900 col-span-9">
+                    <div className={`col-span-9`}>
+                        <div className={`${scroll_height == 0 ? 'h-full' : 'h-1/2'} bg-zinc-600 w-full h-full transition-all duration-100`}>
 
+                        </div>
                     </div>
-                    <div className="col-span-9 row-span-6 flex items-center justify-start flex-col overflow-y-scroll">
+                    <div id="post-window" onScroll={() => handle_nav_bar()} className="col-span-9 row-span-6 flex items-center justify-start flex-col overflow-y-scroll">
                         <PostTemplate />
                         <PostTemplate />
                         <PostTemplate />
